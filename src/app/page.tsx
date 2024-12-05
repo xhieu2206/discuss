fef7f8f9349289c9d887f6fd7341b26a8c2e3cb0
Ross Nguyen <xhieu94@gmail.com>
2024-12-05 19:50:01 +0700

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
 import { Button } from '@nextui-org/react';
 import * as actions from '@/actions';
+import { auth } from '@/auth';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
       <form action={actions.signIn}>
-        <Button>Sign In</Button>
+        <Button type='submit'>Sign In</Button>
       </form>
 
       <form action={actions.signOut}>
-        <Button>Sign Out</Button>
+        <Button type='submit'>Sign Out</Button>
       </form>
+
+      {
+        session?.user ? <div>Signed In</div> : <div>Signed out</div>
+      }
     </div>
   );
 }
